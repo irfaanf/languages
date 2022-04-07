c867e6563ad532f7b1a98a9ea43f3cf95264c96f
Irfaan Fakir <irfaan@hotmail.co.za>
2022-04-07 14:14:35 +0200

    Simplified conversion logic.

--- src/capture/CaptureWord.js
+++ src/capture/CaptureWord.js
@@ -13,78 +13,57 @@ import LanguagesDropdown from "./languages-dropdown/LanguagesDropdown";
 import { createClient } from '@supabase/supabase-js'
 
 export const CaptureWord = () => {
-    const [books, setBooks] = useState([]);
-    const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY)
+    const [bookOptions, setBookOptions] = useState([]);
 
     useEffect(() => {
         getBooks();
-
-        // getBooks().then((result) => {
-        //     console.log(`RESULT:  ${JSON.stringify(result)}`);
-        //     setBooks(result);
-        //     console.log(`BOOKS AFTER RESULT:  ${JSON.stringify(books)}`);
-        // });
-
-        // this.books = this.books.bind();
     }, []);
 
-       
     const getBooks = async () => {
+        const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY);
         let { data } = await supabase
             .from('books')
-            .select('*')
+            .select('*');
 
-        // console.log(`STRINGIFY:  ${JSON.stringify(books)}`);
-        // console.log(`RAW:  ${books}`);
-        // console.log(`ARRAY:  ${JSON.stringify(Array.from(books))}`);
-        // console.log(`[0]:  ${JSON.stringify(books[0])}`);
-        // console.log(`[0] CONVERTED:  ${JSON.stringify(convertDataToOption(books[0]))}`);
-        // console.log(`[1] CONVERTED:  ${JSON.stringify(convertDataToOption(books[1]))}`);
-        // console.log(`[2] CONVERTED:  ${JSON.stringify(convertDataToOption(books[2]))}`);
-        // console.log(`TYPEOF:  ${typeof(books)}`);
-
-        setBooks(data);
-
-        return books;
-    }
-
-    const convertDataToOption = (book) => {
-        console.log("CONVERT======");
-
-        let optionBook = {
-            key: book.id,
-            value: book.title,
-            text: book.title
-        }
-        console.log(`OPTIONBOOK:  ${JSON.stringify(optionBook)}`);
-
-        return optionBook;
-    }
+        setBookOptions(convertAll(data));
+    };
 
     const convertAll = (books) => {
         let result = [];
 
         for (let book in books) {
-            result.push(convertDataToOption(book));
-        }
-        return result;
-    }
+            let optionBook = {
+                key: book.id,
+                value: book.title,
+                text: book.title
+            };
 
-    // const countries = [
-    //     { id: 1, title: 'AFG', slug: 'someslug' },
-    //     { id: 2, title: 'GBR', slug: 'anotherslug' },
-    // ]
-    const countryOptions = [
-        { key: 'af', value: 'af', text: 'Afghanistan' },
-        { key: 'ax', value: 'ax', text: 'Aland Islands' },
+            result.push(optionBook);
+        }
+
+        console.log(`result:  ${JSON.stringify(result)}`);
+        return result;
+
+        // books.map((book) => {
+        //     let optionBook = {
+        //         key: book.id,
+        //         value: book.title,
+        //         text: book.title
+        //     };
+
+        //     return optionBook;
+        // });
+
+        // return books;
+    };
+
+    const dummyOptions = [
+        { key: 1, value: "Die Poort", text: "Die Poort" },
+        { key: 2, value: "Die Ongelooflike Avonture van Hanna Hoekom", text: "Die Ongelooflike Avonture van Hanna Hoekom" },
+        { key: 3, value: "Die Vrou op die Skuit", text: "Die Vrou op die Skuit" }
     ]
 
-    const booksConverted = convertAll(books);
-    console.log(`ISARRAY booksConverted:  ${Array.isArray(booksConverted)}`);
-    console.log(`ISARRAY countryOptions:  ${Array.isArray(countryOptions)}`);
-
-    console.log(`BOOKS CONVERTED:  ${JSON.stringify(booksConverted)}`)
-    console.log(`COUNTRY OPTIONS:  ${JSON.stringify(countryOptions)}`)
+    console.log(React.version);
 
     return (
         <>
@@ -94,43 +73,26 @@ export const CaptureWord = () => {
 
             <Form>
                 <Grid container textAlign="left">
-                    <Grid.Column width="4">
+                    {/* <Grid.Column width="4">
                         <Form.Field>
                             <label>Book</label>
                         </Form.Field>
-                    </Grid.Column>
+                    </Grid.Column> */}
 
                     <Grid.Column width="12">
                         <Form.Field>
                             <Dropdown
                                 name="book-select"
                                 placeholder="Click to select book ..."
-                                options={booksConverted}
-                                // options={(books) => {
-                                //     console.log(`INSIDE`)
-                                //     if (!books) {
-                                //         console.log(`EMPTY`)
-                                //     } else {
-                                //         console.log(`FULL`)
-                                //         for (let b of books) {
-                                //             convertDataToOption(b)
-                                //         }
-                                //     }
-                                // }}
-                                // options={(Array.from(books)).map((b) => {
-                                //     return {
-                                //         key: b.id,
-                                //         value: b.title,
-                                //         text: b.title
-                                //     }
-                                // })}
+                                options={bookOptions}
                                 selection
+                                fluid
                                 className="book-select book-title"
                             />
                         </Form.Field>
                     </Grid.Column>
 
-                    <Grid.Column width="4">
+                    {/* <Grid.Column width="4">
                         <Form.Field inline>
                             <label>Page</label>
                         </Form.Field>
@@ -221,7 +183,7 @@ export const CaptureWord = () => {
                     <Grid.Column width="4"></Grid.Column>
                     <Grid.Column width="8">
                         <Button type="submit">Add</Button>
-                    </Grid.Column>
+                    </Grid.Column> */}
                 </Grid>
             </Form>
         </>
