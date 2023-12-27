137e5247783c3cc6a5020d27a163432efc164cbd
Irfaan Fakir <irfaan.fakir@angloamerican.com>
2023-12-27 18:24:37 +0200

    Created Dropdown component

---
+++ src/components/atoms/Dropdown/Dropdown.jsx
@@ -0,0 +1,11 @@
+export const Dropdown = () => {
+    return (
+        <>
+            <select className="border border-gray-500 w-200 col-span-3 p-2 rounded appearance-none">
+                <option>Die Poort</option>
+                <option>The Hunger Games</option>
+                <option>Die Poort</option>
+            </select>
+        </>
+    );
+};
